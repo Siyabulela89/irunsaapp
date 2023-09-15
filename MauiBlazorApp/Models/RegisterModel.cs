a5f0e3e354f2b4658bff7e2b3bcab22252db03a1
Siyabulela Sinqe <siya42@live.co.za>
2023-09-15 05:44:04 +0200

    Nazo

--- MauiBlazorApp/Models/RegisterModel.cs
+++ MauiBlazorApp/Models/RegisterModel.cs
@@ -5,10 +5,18 @@ namespace irunsaapp.Models
 {
     public class RegisterModel
     {
+
+        [Required]
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+        [Required]
+        [Display(Name = "Surname")]
+        public string? Surname { get; set; }
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string? Email { get; set; }
+
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
