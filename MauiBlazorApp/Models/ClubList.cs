7487e385ec63f563468f7c0bebca9b5529c588f7
Siyabulela Sinqe <siya42@live.co.za>
2023-09-20 06:17:18 +0200

    Changes for the night athlete profile

---
+++ MauiBlazorApp/Models/ClubList.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace irunsaapp.Models
+{
+    public class ClubList
+    {
+     
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClubListId { get; set; }
+        public string Club { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ClubListId} - {Club}";
+        }
+
+
+    }
+}
